e5bdc91c0f3c48c2b094b8958dc1b555a58477f6
Jean Loriston <staticking29@gmail.com>
2024-02-09 14:51:25 -0500

    attempt to  disable cors

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors'); // Import the cors middleware
 const app = express();
 const port = process.env.PORT || 3000; // Use the provided port in Cloud Run
-
-// Use the cors middleware to allow all origins
-app.use(cors());
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'dist'))); // Updated path
