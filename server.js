76dd1a20574eef7d4304a67d7f7e180bc0ac21db
Jean Loriston <staticking29@gmail.com>
2024-02-09 15:06:09 -0500

    attempt to fix cors

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors'); // Import the cors middleware
 const app = express();
 const port = process.env.PORT || 3000; // Use the provided port in Cloud Run
 
-// Allow CORS from anywhere
-app.use(cors());
-
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist'))); // Updated path
 
 // Handles any requests that don't match the above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html')); // Updated path
 });
 
 app.listen(port, () => {
